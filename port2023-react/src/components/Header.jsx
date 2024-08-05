1ec5792473afff555402290006c32f62efc8a850
unknown <tndusslarj@gmail.com>
2024-08-05 17:21:22 +0900

    헤더영역 완료

--- port2023-react/src/components/Header.jsx
+++ port2023-react/src/components/Header.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+const headerNav = [
+  {
+      title: "intro",
+      url: "#intro",
+  },
+  {
+      title: "skill",
+      url: "#skill",
+  },
+  {
+      title: "site",
+      url: "#site",
+  },
+  {
+      title: "portfolio",
+      url: "#port",
+  },
+  {
+      title: "contact",
+      url: "#contact",
+  },
+];
 
 const Header = () => {
+  const[show, setShow] = useState(false);
+
+  const toggleMenu = () => {
+    setShow((prevShow) => !prevShow);
+  };
+  
   return (
-    <div>
-      Header
-    </div>
+    <header id="header" role="banner">
+      <div className="header__inner">
+        <div className="header__logo">
+          <a href="/">portfolio<em>vute</em></a>
+        </div>
+        <nav className="header__nav" role="navigation" aria-label="메인메뉴">
+          <ul>
+            {headerNav.map((nav,key) =>(
+              <li key={key}>
+                <a href={nav.url}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className='"header__nav__mobile'
+             id="headerToggle"
+             aria-controls='primary-menu'
+             aria-expanded="false"
+             role='button'
+             tabIndex="0">
+              <span></span>
+        </div>
+      </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
