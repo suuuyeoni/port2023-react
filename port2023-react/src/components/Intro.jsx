f4c2507873697a0f37ea0e52e1667bf55683dddd
unknown <tndusslarj@gmail.com>
2024-08-09 16:56:48 +0900

    인트로 완료

--- port2023-react/src/components/Intro.jsx
+++ port2023-react/src/components/Intro.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from "react"
+
+import about from "../assets/img/about.png";
+
+const introText = {
+  title: "LEE DOBORN",
+  desc : ["talent is","found at the end of the","effort"]
+}
 
 const Intro = () => {
   return (
-    <div>
-      Intro
-    </div>
+    <section id="intro">
+      <div className="intro__inner">
+        <h2 className="intro__title">
+          {introText.title}
+        </h2>
+        <div className="intro__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="intro__text">
+          <div className="text">
+            <div>{introText.desc[0]}</div>
+            <div>{introText.desc[1]}</div>
+            <div>{introText.desc[2]}</div>
+          </div>
+          <div className="img">
+            <img src={about} alt="어바웃미"/>
+          </div>
+        </div>
+        <div className="intro__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
   )
 }
 
